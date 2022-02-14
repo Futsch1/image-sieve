aa2a77e73ecf21c196643c263a1382d3432db4c3
Florian Fetz <florian.fetz@googlemail.com>
2022-02-14 20:12:52 +0100

    Force fluent style #30

--- build.rs
+++ build.rs
@@ -1,8 +1,12 @@
 extern crate winres;
 
 fn main() {
-    println!("slint  build");
-    slint_build::compile("ui/image_sieve.slint").unwrap();
+    println!("slint build");
+    slint_build::compile_with_config(
+        "ui/image_sieve.slint",
+        slint_build::CompilerConfiguration::new().with_style(String::from("fluent")),
+    )
+    .unwrap();
     if cfg!(target_os = "windows") {
         let mut res = winres::WindowsResource::new();
         res.set_icon("ImageSieve.ico");
