203901f654787804470e8fe158680f165a6884f7
Florian Fetz <63090558+Futsch1@users.noreply.github.com>
2023-08-13 17:22:57 +0200

    devcontainer (#81)
    * added devcontainer
    
    * fixes for devcontainer
    
    * add clang
    
    * add host requirements
    
    * increase requirement to 16gb
    
    * added sshd

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,36 @@
+## update and install some things we should probably have
+apt-get update
+apt-get install -y \
+  curl \
+  git \
+  gnupg2 \
+  jq \
+  sudo \
+  zsh \
+  vim \
+  build-essential \
+  openssl \
+  libxcb-shape0-dev \
+  libxcb-xfixes0-dev \
+  libxkbcommon-dev \
+  libgtk-3-dev \
+  libavcodec-dev \
+  libavformat-dev \
+  libavutil-dev \
+  libswscale-dev \
+  clang
+
+## Install rustup and common components
+curl https://sh.rustup.rs -sSf | sh -s -- -y 
+source "$HOME/.cargo/env"
+rustup install
+rustup component add rustfmt
+rustup component add clippy 
+rustup component add llvm-tools-preview
+
+## setup and install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+cp -R /root/.oh-my-zsh /home/$USERNAME
+cp /root/.zshrc /home/$USERNAME
+sed -i -e "s/\/root\/.oh-my-zsh/\/home\/$USERNAME\/.oh-my-zsh/g" /home/$USERNAME/.zshrc
+chown -R $USER_UID:$USER_GID /home/$USERNAME/.oh-my-zsh /home/$USERNAME/.zshrc
