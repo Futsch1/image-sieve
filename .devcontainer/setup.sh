464465e86991d9d34f3345ff1179ab8c9e0404a3
Futsch1 <florian.fetz@googlemail.com>
2023-08-15 19:31:39 +0000

    Remove zsh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -28,10 +28,3 @@ source "$HOME/.cargo/env"
 rustup component add rustfmt
 rustup component add clippy 
 rustup component add llvm-tools-preview
-
-## setup and install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-cp -R /root/.oh-my-zsh /home/$USERNAME
-cp /root/.zshrc /home/$USERNAME
-sed -i -e "s/\/root\/.oh-my-zsh/\/home\/$USERNAME\/.oh-my-zsh/g" /home/$USERNAME/.zshrc
-chown -R $USER_UID:$USER_GID /home/$USERNAME/.oh-my-zsh /home/$USERNAME/.zshrc
