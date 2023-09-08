df1e76a40099982cee9bd6276924307486ea6f2d
Futsch1 <florian.fetz@googlemail.com>
2023-09-08 15:12:03 +0000

    Added emoji subset

--- ui/fonts/generateFont.sh
+++ ui/fonts/generateFont.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
+rm *.ttf
+python3 generateSymbols.py
 wget https://github.com/googlefonts/noto-emoji/raw/main/fonts/NotoColorEmoji.ttf
-wget https://github.com/notofonts/notofonts.github.io/raw/main/fonts/NotoSans/full/ttf/NotoSans-Regular.ttf
 pyftsubset NotoColorEmoji.ttf --unicodes-file=symbols.txt --output-file=NotoColorEmoji.subset.ttf
-#ttx -o NotoImageSieve.ttf -m NotoColorEmoji.subset.ttf NotoSans-Regular.ttx
-#ttx NotoColorEmoji.subset.ttf
-ttx NotoSans-Regular.otf
-ttx -o NotoSans-Regular.patched.ttf NotoSans-Regular.ttx
-#ttx -o NotoImageSieve.ttf -m NotoSans-Regular.ttf NotoColorEmoji.subset.ttx 
-pyftmerge NotoSans-Regular.patched.ttf NotoColorEmoji.subset.ttf --output-file=NotoImageSieve.ttf
--- ui/fonts/generateSymbols.py
+++ ui/fonts/generateSymbols.py
@@ -1,3 +1,10 @@
 with open('../help.slint') as f:
     content = f.read()
-    
+    symbols = set()
+    for char in content:
+        if ord(char) > 127:
+            symbols.add(ord(char))
+
+with open('symbols.txt', 'w') as f:
+    f.write('\n'.join([f'U+{symbol:X}' for symbol in sorted(list(symbols))]))
+    f.close()
